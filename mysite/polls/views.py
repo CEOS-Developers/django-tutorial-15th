038a82cf4235e1c6d7d1830aa91475ffa9fecba4
yesjjin99 <yesjjin99@naver.com>
2022-03-16 05:17:11 +0900

    feat: Add django tutorial part.2

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-  return HttpResponse("Hello, World. You're at the polls index.")
+	return HttpResponse("Hello, World. You're at the polls index.")
