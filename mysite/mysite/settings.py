81fca7eb87c34d3d2c0c283abbaf96644db1e920
offsujin <offsujin@gmail.com>
2022-03-17 19:04:11 +0900

    Add Question in polls/admin.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cgl9)a@#l)1b56i0w=_pldwth3w3(*2@2s_bq#^ye*!_c$2h9z'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
