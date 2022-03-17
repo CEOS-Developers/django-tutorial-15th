454f47699caa2f53b16c28fc4a19095d3de59029
offsujin <offsujin@gmail.com>
2022-03-17 19:16:56 +0900

    Add detail, results, vote view

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/3/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/3/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/3/vote/
+    path('<int:question_id>/vote', views.vote, name='vote'),
 ]
