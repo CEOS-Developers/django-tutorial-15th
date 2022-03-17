5e3ad2e7bd4cab9ee37fccc4d920529d4bef3d6a
zinc <your_zinc@ewhain.net>
2022-03-17 14:50:53 +0900

    (1주차) Django Tutorial Part. 2

--- README.md
+++ README.md
@@ -6,10 +6,32 @@ CEOS 15th 백엔드 Django-tutorial
 2. git status - .idea/ 폴더의 정체 = pycharm의 회사 IntelliJ 프로젝트별 생성파일
    (git ignore 에 추가해야 한다는 의견과 그렇지 않다는 의견이 갈린다)
 ---
+
+
 #### [1주차] 과제
+
+      [1] 
+
 1. `python manage.py runserver` 
 2. `python manage.py runserver 8080` pass the port number
 3. To call the view(view.py), we need to map it to a URL(urls.py) - URLconf.
 4. `include()` : including other URL patterns. (only exception -`admin.site.urls`)
 5. `path()` : `path(route, view, kwargs, name)`
 6. path() argument: (required) route, view
+
+      
+      [2] Database Setting
+
+8. `TimeZone` : setting.py -> `TIME_ZONE ='Asia/Seoul'`, `USE_TZ = False`
+9. `python manage.py migrate` :  creates any necessary database tables
+10. `models.CharField(max_length=)` : `max_length` is required
+
+
+11. Model 변경 
+- Modify models.py 
+
+- `python manage.py makemigrations polls`
+
+- `python manage.py migrate`
+12. [ DATABASE API ] : use double underscores to separate relationships.
+e.g ) `question__pub_date__year`, `choice_text__startswith`
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -27,16 +26,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
+    'django.contrib.admin',  # 관리용 사이트
+    'django.contrib.auth',  # 인증 시스템
+    'django.contrib.contenttypes',  # 컨텐츠 타입을 위한 프레임워크
+    'django.contrib.sessions',  # 세션 프레임워크
+    'django.contrib.messages',  # 메세징 프레임워크
+    'django.contrib.staticfiles',   # 정적 파일을 관리하는 프레임워크
 ]
 
 MIDDLEWARE = [
@@ -69,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -79,7 +77,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -99,20 +96,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'  # 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
-
+USE_TZ = False  # True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
