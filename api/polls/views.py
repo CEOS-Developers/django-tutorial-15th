6b9d4c92b79f92653f830f462ddf24b66885631e
김민준 <sanbonai06@naver.com>
2022-03-20 22:40:30 +0900

    리뷰 수정

--- api/polls/views.py
+++ api/polls/views.py
@@ -26,4 +26,16 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
-    ... # same as above, no changes needed.
+    def vote(request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        except (KeyError, Choice.DoesNotExist):
+            return render(request, 'polls/detail.html', {
+                'question': question,
+                'error_message': "You didn't select a choice.",
+            })
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
